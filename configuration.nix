7d0211772aba5e32d0a8cda82e6d37b14eb3dbfb
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-22 21:56:53 +0530

    Updated

--- configuration.nix
+++ configuration.nix
@@ -10,13 +10,27 @@
       ./hardware-configuration.nix
     ];
 
-  # Add Xanmod Kernel - MR - 22-02
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
   # bootspec needed for secureboot - MR 22-02
   boot.bootspec.enable = true;
+  # Add Xanmod Kernel - MR - 22-02
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  # Bootloader - disable systemd in favor of lanzaboote
+  boot.loader.systemd-boot.enable = pkgs.lib.mkForce false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # bootloader timeout set, also press t repeatedly in the bootmenu to set there
+  boot.loader.timeout = 15;
+
+  # lanzaboote for secureboot
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
+
+  # plymouth theme for splash screen
+  boot.kernelParams = [ "quiet" "lockdown=integrity" ];
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "breeze";
+  boot.initrd.systemd.enable = true;
 
   # enable bluetooth support
   hardware.bluetooth.enable = true; # enables support for Bluetooth
--- flake.nix
+++ flake.nix
@@ -4,16 +4,20 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    lanzaboote.url = "github:nix-community/lanzaboote";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, lanzaboote, ... }:
     let
       lib = nixpkgs.lib;
     in {
     nixosConfigurations = {
       Ainz-NIX = lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules = [
+          ./configuration.nix
+          lanzaboote.nixosModules.lanzaboote # lanzaboote for secureboot
+        ];
       };
     };
   };
