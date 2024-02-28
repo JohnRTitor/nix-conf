bcddf6cec39a50acecea54a4672d5991342d0fc1
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-28 07:10:03 +0000

    Still does not build glibc due to locale

--- system/locale.nix
+++ system/locale.nix
@@ -11,13 +11,13 @@
   i18n.extraLocaleSettings = {
     LC_ADDRESS = systemSettings.locale;
     LC_IDENTIFICATION = systemSettings.locale;
-    LC_MEASUREMENT = systemSettings.locale;
-    LC_MONETARY = systemSettings.localeoverride;
+    LC_MEASUREMENT = systemSettings.localeoverride;
+    LC_MONETARY = systemSettings.locale;
     LC_NAME = systemSettings.locale;
     LC_NUMERIC = systemSettings.locale;
-    LC_PAPER = systemSettings.localeoverride;
-    LC_TELEPHONE = systemSettings.localeoverride;
-    LC_TIME = systemSettings.localeoverride;
+    LC_PAPER = systemSettings.locale;
+    LC_TELEPHONE = systemSettings.locale;
+    LC_TIME = systemSettings.locale;
   };
 
   i18n.supportedLocales = [
