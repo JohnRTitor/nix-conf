2e8c37175e5ec823c7744d9bf1bd68290d23ae14
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-28 06:20:26 +0000

    Update flake

--- flake.nix
+++ flake.nix
@@ -14,10 +14,7 @@
     # home-manager, used for managing user configuration
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
+      # follow the stable nixpkgs, to ensure compatibility
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
