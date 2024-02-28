2bbd6e8ee98b2df65199f36c6ba8795bba7489ac
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-28 17:49:07 +0000

    amdgpu pro

--- system/boot.nix
+++ system/boot.nix
@@ -6,8 +6,6 @@
   boot.bootspec.enable = true;
   # Add Xanmod Kernel - MR - 22-02
   boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-  # Also load amdgpu at boot
-  boot.kernelModules = [ "amdgpu-pro" ];
   # Bootloader - disable systemd in favor of lanzaboote
   boot.loader.systemd-boot.enable = pkgs.lib.mkForce false;
   boot.loader.efi.canTouchEfiVariables = true;
