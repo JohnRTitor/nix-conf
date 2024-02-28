36516b5094c25cfd211a5ce4f487c8a2ce630394
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-28 15:22:39 +0000

    Fix bug

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@
   imports = [
     # pyprland config wrapper
     ./home-manager/pyprland.nix
-  ]
+  ];
 
   # link the configuration file in current directory to the specified location in home directory
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
