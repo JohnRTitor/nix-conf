a4fbaaaaac8a5d8df286ac60a23576e02c08ef4e
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-28 07:23:21 +0000

    Fix bug

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         system = "x86_64-linux"; # system arch
         hostname = "Ainz-NIX"; # hostname
         timezone = "Asia/Kolkata"; # select timezone
-        locale = ""en_US.UTF-8""; # select locale
+        locale = "en_US.UTF-8"; # select locale
         localeoverride = "en_IN";
         stableversion = "23.11";
       };
