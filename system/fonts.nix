d9cdb16c7b4f7100e25afa56c4d0dd399510c00e
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-02-29 23:15:52 +0530

    Bug fix

--- system/fonts.nix
+++ system/fonts.nix
@@ -8,6 +8,6 @@
     noto-fonts-cjk
     jetbrains-mono
     font-awesome
-    (nerdfonts.override {fonts = [ "JetBrainsMono" "nconsolataLGC" ];})
+    (nerdfonts.override {fonts = [ "JetBrainsMono" "InconsolataLGC" ];})
   ];
 }
