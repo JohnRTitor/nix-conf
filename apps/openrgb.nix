5445565f063f1b727e5f23cbae4de2e23763437e
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-03 09:26:25 +0530

    Update openrgb

--- apps/openrgb.nix
+++ apps/openrgb.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.openrgb-with-all-plugins ];
-
   # OpenRGB setup
   services.hardware.openrgb = {
     enable = true;
     motherboard = "amd";
+    package = pkgs.openrgb-with-all-plugins; # enable all plugins
   };
 }
