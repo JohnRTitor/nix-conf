4df1e94b5744054862a9d5de5016f75bad3e5bce
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-03 23:30:33 +0530

    Fix

--- apps/openrgb.nix
+++ apps/openrgb.nix
@@ -8,5 +8,5 @@
     package = pkgs.openrgb-with-all-plugins; # enable all plugins
   };
   environment.systemPackages = [ pkgs.i2c-tools ];
-  users.groups.i2c.members = [ userSettings.name ]; # create i2c group and add default user to it
+  users.groups.i2c.members = [ userSettings.username ]; # create i2c group and add default user to it
 }
