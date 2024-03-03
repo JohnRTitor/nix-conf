d3a176167a8adb8fdfcb4bf758d015ec172d9faf
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-03 16:03:23 +0530

    Disable virtualbox

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -2,9 +2,15 @@
 { config, pkgs, userSettings, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ virt-manager virtualbox distrobox ];
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    # virtualbox
+    distrobox
+  ];
   virtualisation.libvirtd = {
     enable = true;
+    onShutdown = "shutdown"; # Shutdown VMs on host shutdown
+    spiceUSBRedirection.enable = true; # allows VMs to access USB
     qemu.runAsRoot = false;
     allowedBridges = [
       "nm-bridge"
@@ -12,5 +18,5 @@
     ];
   };
   users.users.${userSettings.username}.extraGroups = [ "libvirtd" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ virtualbox ];
+  # boot.extraModulePackages = with config.boot.kernelPackages; [ virtualbox ];
 }
