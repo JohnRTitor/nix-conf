0bb0a34f63b92b73100765a2888eadc7bbca640b
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-04 23:43:51 +0530

    Remove shell alias

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -14,9 +14,7 @@
 
     # set some aliases, feel free to add more or remove some
     shellAliases = {
-      k = "kubectl";
-      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+      # k = "kubectl";
     };
   };
 
