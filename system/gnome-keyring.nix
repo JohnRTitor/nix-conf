62273fe4d2a432b90d5808055202316f992a89aa
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-06 11:58:02 +0530

    Enable GNOME keyring at startup

--- system/gnome-keyring.nix
+++ system/gnome-keyring.nix
@@ -6,4 +6,5 @@
   services.gnome.gnome-keyring.enable = true;
   programs.seahorse.enable = true; # enable the graphical frontend
   environment.systemPackages = [ pkgs.libsecret ]; # libsecret api needed
+  security.pam.services.gdm.enableGnomeKeyring = true; # load gnome-keyring at startup
 }
