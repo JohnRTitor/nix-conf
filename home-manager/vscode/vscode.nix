2092eb69c80a4467f0f0676e25457c856eaa55cc
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-16 00:15:25 +0530

    VS code configuration added

--- home-manager/vscode/vscode.nix
+++ home-manager/vscode/vscode.nix
@@ -1,6 +1,30 @@
 # this config file is a wrapper to automatically configure vscode via a config file
-{ ... }:
+{ pkgs, ... }:
 {
+  programs.vscode = {
+    enable = true;
+    enableUpdateCheck = false;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix # Nix language support
+      ms-python.python # Python language support
+      ms-vscode.cpptools # C/C++ language support
+      tamasfe.even-better-toml # TOML language support
+
+      dracula-theme.theme-dracula # Dracula theme
+
+      github.copilot # GitHub Copilot
+      github.copilot-chat # GitHub Copilot Chat
+      github.codespaces # GitHub Codespaces
+      github.vscode-pull-request-github # GitHub Pull Requests
+
+      ms-azuretools.vscode-docker # Docker
+      ms-vscode-remote.remote-ssh # Remote SSH
+    ];
+    userSettings = {
+      "git.confirmSync" = false; # Do not ask for confirmation when syncing
+      "git.autofetch" = true; # Periodically fetch from remotes
+    };
+  };
   home.file.".vscode/argv.json" = {
     source = ./vscode-argv.json5;
     executable = false;
