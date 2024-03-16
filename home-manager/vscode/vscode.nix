05f0795e7082ffb8d44bfb22a87752852962114c
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-16 22:00:30 +0530

    Revert "Add pastebin extension"
    This reverts commit 1dcd59d5d2f60e48f7d134ebdc1030f8e04dac8e.

--- home-manager/vscode/vscode.nix
+++ home-manager/vscode/vscode.nix
@@ -19,8 +19,6 @@
 
       ms-azuretools.vscode-docker # Docker
       ms-vscode-remote.remote-ssh # Remote SSH
-
-      rolandgreim.sharecode # Paste code to Github, Pastebin
     ];
     userSettings = {
       "git.confirmSync" = false; # Do not ask for confirmation when syncing
