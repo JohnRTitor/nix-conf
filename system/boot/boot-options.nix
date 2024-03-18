cc88732f0469871a9dfbb0b2af62d9c4c7f32018
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-18 21:53:26 +0530

    Disable hibernate since its not possible in zram swap

--- system/boot/boot-options.nix
+++ system/boot/boot-options.nix
@@ -14,6 +14,7 @@
 
   # boot.consoleLogLevel = 0; # configure silent boot
   boot.kernelParams = [
+    "nohibernate" # disable hibernate, since you can't on zram swap anyways
     # "acpi_enforce_resources=lax" # openrgb
     # "quiet"
     # "udev.log_level=3"
