ce8a9ba4a586fb89606992b2a3cef773a223bd68
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-18 16:04:01 +0530

    Removed because these options are not even being applied

--- system/boot/kernel.nix
+++ system/boot/kernel.nix
@@ -6,7 +6,7 @@
   
   boot.extraModulePackages = with config.boot.kernelPackages; [
     # zenpower is used for reading temperature, voltage, current and power
-    # zenpower # disabled because k10temp is enough
+    zenpower
   ];
 
   # List of patches to compile the kernel with
@@ -41,10 +41,6 @@
         KERNEL_GZIP = unset;
         KERNEL_ZSTD = yes;
 
-        # POSIX Message Queues disabled - only needed in Solaris
-        POSIX_MQUEUE = lib.mkForce unset;
-        POSIX_MQUEUE_SYSCTL = lib.mkForce unset;
-
         # Kernel optimized for MORE performance
         CC_OPTIMIZE_FOR_PERFORMANCE = unset;
         CC_OPTIMIZE_FOR_PERFORMANCE_O3 = yes;
@@ -58,44 +54,11 @@
         X86_USE_PPRO_CHECKSUM = yes;
         
         NR_CPUS = lib.mkForce (freeform "20"); # only 20 threads support
-        X86_MPPARSE = unset; # MPS table is not needed for new systems with ACPI support
-
-        # Disable intel specific services
-        X86_MCE_INTEL = unset; # disable Intel MCE
-        PERF_EVENTS_INTEL_UNCORE = unset; # disable intel UNCORE perf monitor
-        PERF_EVENTS_INTEL_CSTATE = unset; # disable CSTATE intel
-
-        X86_EXTENDED_PLATFORM = unset; # disable other X86 platforms
-        
-        # Enable AMD power monitors
-        PERF_EVENTS_AMD_POWER = module; # load as a module
-        PERF_EVENTS_AMD_BRS = yes;
-
-        # Enable AMD SME
-        DYNAMIC_PHYSICAL_MASK = yes; # for SME
-        X86_MEM_ENCRYPT = yes;
-        AMD_MEM_ENCRYPT = yes;
-        AMD_MEM_ENCRYPT_ACTIVE_BY_DEFAULT = unset;
-        ARCH_HAS_CC_PLATFORM = yes;
-        UNACCEPTED_MEMORY = yes;
-        ARCH_HAS_FORCE_DMA_UNENCRYPTED = yes;
-        DMA_COHERENT_POOL = yes;
-
 
         ##### MEMORY MANAGEMENT #####
         # Multigen LRU
         LRU_GEN = yes;
         LRU_GEN_ENABLED = yes;
-
-        ##### DEVICE DRIVERS #####
-        MACINTOSH_DRIVERS = no;
-
-        # TODO: Disable a lot of unneeded drivers
-
-        # Disable miscellaneous filesystem
-        MISC_FILESYSTEMS = unset;
-        # Disable kernel debugging
-        DEBUG_KERNEL = lib.mkForce unset;
       };
     }
   ];
