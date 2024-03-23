1e078f665874782480849d6967dac9adf2ffce51
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-23 23:35:13 +0530

    Reenable gpu driver

--- home-manager/neofetch/neofetch.nix
+++ home-manager/neofetch/neofetch.nix
@@ -1,7 +1,12 @@
 # this config file is a wrapper to automatically configure neofetch
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.neofetch ];
+  home.packages = with pkgs; [
+    neofetch
+    psmisc # fuser command (used by neofetch)
+    lsof # list open files (used by neofetch)
+  ];
+
   home.file.".config/neofetch/config.conf".source = ./neofetch-default.conf;
   home.file.".config/neofetch/config-compact.conf".source = ./neofetch-compact.conf;
   home.file.".config/neofetch/config.conf.save".source = ./neofetch-config.conf.save;
