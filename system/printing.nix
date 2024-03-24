b3b717eebd0fa3704bff1e8cb0ecdddd7dc6a486
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-25 00:06:16 +0530

    Update printing settings

--- system/printing.nix
+++ system/printing.nix
@@ -4,8 +4,10 @@
 {
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.cups-pdf.enable = true; # Enable PDF printing.
+  services.printing.openFirewall = true; # Open ports for printing
+  # Enable Avahi to discover printers, and LAN devices
   services.avahi.enable = true;
   services.avahi.nssmdns4 = true;
   services.avahi.openFirewall = true;
-  environment.systemPackages = [ pkgs.cups-filters ];
 }
