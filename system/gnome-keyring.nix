546808fc863cf244efc0b639a54f7c8e0c509da4
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-26 00:28:59 +0530

    Fix GNOME keryring not being unlocked

--- system/gnome-keyring.nix
+++ system/gnome-keyring.nix
@@ -5,6 +5,7 @@
 {
   services.gnome.gnome-keyring.enable = true;
   environment.systemPackages = [ pkgs.libsecret ]; # libsecret API
-  security.pam.services.login.enableGnomeKeyring = true; # load gnome-keyring at startup
+  security.pam.services.gdm.enableGnomeKeyring = true; # load gnome-keyring at startup
+  environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID"; # set the runtime directory
   programs.seahorse.enable = true; # enable the graphical frontend for managing
 }
