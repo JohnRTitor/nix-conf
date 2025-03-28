505302d9284ed4811f211b4db5892074c9c0f426
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-28 17:31:17 +0530

    add amd settings, update-flake
    enable zenpower - remove the override in the next nixpkgs bump
    enable amd-pstate
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

---
+++ system/hardware/amd.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, pkgs-master, ... }:
+{
+  boot.kernelParams = [
+    # https://docs.kernel.org/admin-guide/pm/amd-pstate.html#active-mode
+    "amd-pstate=active"
+  ];
+
+  # Zenpower is better power reporting module for AMD processors
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    (zenpower.overrideAttrs {
+      version = "unstable-2025-02-28";
+
+      src = pkgs.fetchFromGitLab {
+        owner = "shdwchn10";
+        repo = "zenpower3";
+        rev = "138fa0637b46a0b0a087f2ba4e9146d2f9ba2475";
+        hash = "sha256-kLtkG97Lje+Fd5FoYf+UlSaEyxFaETtXrSjYzFnHkjY=";
+      };
+    })
+  ];
+  boot.blacklistedKernelModules = [ "k10temp" ];
+}
