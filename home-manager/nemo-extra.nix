385e4d078669ec8b7e75b4a167b7abfb678d7da4
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-29 08:31:09 +0530

    home-manager: nemo stuff
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

---
+++ home-manager/nemo-extra.nix
@@ -0,0 +1,5 @@
+{config, lib, pkgs, ...}:
+{
+  # Most configuration is enabled in system/file-manager
+  home.file.".local/share/nemo/actions/open_in_kitty.nemo_action".source = ./open_in_kitty.nemo_action;
+}
