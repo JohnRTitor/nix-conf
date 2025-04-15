386048c6e0f6345a11a2ec3c22f75bb49138cf43
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-04-15 20:58:44 +0530

    direnv: move to home-manager
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- home-manager/shell/default.nix
+++ home-manager/shell/default.nix
@@ -2,7 +2,7 @@
 # You can add custom aliases, session variables, and other shell configurations here
 # NOTE: related global shell options like programs.zsh.enable must also be added to configuration.nix
 # Else files may not be sourced properly
-{ ... }:
+{ pkgs, ... }:
 let
   inherit (import ./common.nix) commonAliases;
 in
@@ -14,4 +14,17 @@ in
   ];
   # Define common aliases which would apply to all shells
   home.shellAliases = commonAliases;
+
+  # automatically activate direnv on a per directory basis
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+    silent = true;
+  };
+
+  # devenv is a faster and easier way to configure development
+  # environment, better than nix-shell
+  home.packages = [ pkgs.devenv ];
 }
--- system/shell/default.nix
+++ system/shell/default.nix
@@ -5,13 +5,4 @@
   programs.zsh.enable = true;
   # zsh is also enabled for user, conditionally at ../users.nix
   # set the user shell in ../../preferences.nix
-
-  # automatically activate direnv on a per directory basis
-  programs.direnv = {
-    enable = true;
-    silent = true;
-  };
-
-  # devenv a newer way for nix-shell
-  environment.systemPackages = [ pkgs.devenv ];
 }
