53bc83aef18849976d5a42cc727d38dd0e38c5b0
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-05-24 14:18:46 +0530

    gnome-keyring: fix
    the issue was gnome keyring daemon was not properly started on Hyprland
    GNOME does it on its own (without the help of Nix), so this bug could not be reproduced there
    
    thanks to a kind stranger on the internet
    https://discourse.nixos.org/t/gpg-ssh-gnome-keyring-recommendation/48647/8
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- system/services/gnome-keyring.nix
+++ system/services/gnome-keyring.nix
@@ -1,9 +1,20 @@
 # GNOME Keyring for storing/encrypting sycrets
 # apps like vscode stores encrypted data using it
-{ ... }:
+  # NOTE: GNOME keyring does not enable a ssh agent/GPG agent in NixOS
+{ pkgs, ... }:
 {
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.login.enableGnomeKeyring = true; # load gnome-keyring at startup
   programs.seahorse.enable = true; # enable the graphical frontend for managing
-  # NOTE: GNOME keyring does not enable a ssh agent/GPG agent in NixOS
+
+  security.pam.services = {
+    greetd.enableGnomeKeyring = true;
+    greetd-password.enableGnomeKeyring = true;
+    login.enableGnomeKeyring = true;
+  };
+  services.dbus.packages = [ pkgs.gnome-keyring pkgs.gcr ];
+
+  services.xserver.displayManager.sessionCommands = ''
+    eval $(gnome-keyring-daemon --start --daemonize --components=ssh,secrets)
+    export SSH_AUTH_SOCK
+  '';
 }
