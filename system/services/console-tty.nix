bec85908082ccb95f2217cfbc5324af8717099f5
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-08-04 16:03:40 +0530

    system/services/console: start systemd-vconsole-setup after local fs mount
    Else it might fail

--- system/services/console-tty.nix
+++ system/services/console-tty.nix
@@ -1,5 +1,8 @@
 { ... }:
 {
-  console.earlySetup = true; # disabling this may not start vconsole setup
+  console.earlySetup = true;
+  # start after fs have been mounted, else it might fail
+  systemd.services.systemd-vconsole-setup.after = [ "local-fs.target" ];
+
   # services.gpm.enable = true; # For mouse support in tty console, not needed for me
 }
