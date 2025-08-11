4c203e975e54026da3c8557ccf4faf0490aca887
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-08-11 12:49:30 +0530

    switch to nixos-unstable test branch to use sysfs features
    Cherry picked https://github.com/NixOS/nixpkgs/commit/ba04f97d4e4f664a4f9c73cfb01ad9982f936cd6
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

---
+++ home-manager/vesktop.nix
@@ -0,0 +1,20 @@
+{
+  programs.vesktop = {
+    enable = true;
+    settings = {
+      appBadge = false;
+      arRPC = true;
+      checkUpdates = false;
+      customTitleBar = false;
+      disableMinSize = true;
+      minimizeToTray = true;
+      tray = true;
+      splashBackground = "#000000";
+      splashColor = "#ffffff";
+      splashTheming = true;
+      staticTitle = true;
+      hardwareAcceleration = true;
+      discordBranch = "stable";
+    };
+  };
+}
