4068de8964c3dbf88c66992fbf1e73ed78851efa
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-08-12 18:04:24 +0530

    user-packages/masum-work: add gitkraken, update mongodb-compass
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- pkgs/user-packages/masum-work.nix
+++ pkgs/user-packages/masum-work.nix
@@ -5,15 +5,27 @@
     prettier
   ];
 
-  xdg.desktopEntries = {
-    mongodb-compass = {
-      name = "MongoDB Compass";
-      comment = "The MongoDB GUI";
-      genericName = "MongoDB Compass";
-      exec = "${pkgs.mongodb-compass}/bin/mongodb-compass --theme dark --ignore-additional-command-line-flags --enable-features=UseOzonePlatform --ozone-platform=x11 --password-store=gnome-libsecret %U";
-      terminal = false;
-      categories = [ "GNOME" "GTK" "Utility" ];
-      mimeType = [ "x-scheme-handler/mongodb" "x-scheme-handler/mongodb+srv" ];
+  xdg.desktopEntries.mongodb-compass = {
+    name = "MongoDB Compass";
+    comment = "The MongoDB GUI";
+    genericName = "MongoDB Compass";
+    exec = "${pkgs.mongodb-compass}/bin/mongodb-compass --theme dark --ignore-additional-command-line-flags --enable-features=UseOzonePlatform --ozone-platform=x11 --password-store=gnome-libsecret %U";
+    icon = "${pkgs.mongodb-compass}/share/pixmaps/mongodb-compass.png";
+    terminal = false;
+    categories = [ "GNOME" "GTK" "Utility" ];
+    mimeType = [ "x-scheme-handler/mongodb" "x-scheme-handler/mongodb+srv" ];
+  };
+
+  xdg.desktopEntries.gitkraken = {
+    name = "GitKraken Desktop";
+    icon = "${pkgs.gitkraken}/share/gitkraken/gitkraken.png";
+    comment = "Unleash your repo";
+    genericName = "Git Client";
+    exec = "${pkgs.gitkraken}/bin/gitkraken --enable-features=UseOzonePlatform --ozone-platform=x11 --password-store=gnome-libsecret %U";
+    terminal = false;
+    categories = ["Development"];
+    settings = {
+      Version = "1.4";
     };
   };
 }
