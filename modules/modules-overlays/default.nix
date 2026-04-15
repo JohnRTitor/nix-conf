3793f34eca8ebcb952243074e6ae4491c31b0725
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-04-16 01:35:29 +0530

    pipewire: fix where it would play no sound from speakers when headphone is plugged in
    even though we select speaker (line out) specifically from a client like pwvucontrol

--- modules/modules-overlays/default.nix
+++ modules/modules-overlays/default.nix
@@ -20,4 +20,15 @@
     (final: prev: {
     })
   ];
+  
+  system.replaceDependencies.replacements = with pkgs; [
+    {
+      oldDependency = pkgs.pipewire;
+      newDependency = pkgs.pipewire.overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches or [] ++ [
+          ./0001-alsa-acp-don-t-override-user-selected-port-on-availa.patch
+        ];
+      });
+    }
+  ];
 }
