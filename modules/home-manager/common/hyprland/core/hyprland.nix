701d20d69b98e7fd45f1426646902b5cfe805915
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-05-18 18:09:19 +0530

    update flake, migrate hyprland

--- modules/home-manager/common/hyprland/core/hyprland.nix
+++ modules/home-manager/common/hyprland/core/hyprland.nix
@@ -135,7 +135,6 @@ in
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         enable_swallow = false;
-        vfr = true; # Variable Frame Rate
         vrr = 2; # Variable Refresh Rate  Might need to set to 0 for NVIDIA/AQ_DRM_DEVICES
         # Screen flashing to black momentarily or going black when app is fullscreen
         # Try setting vrr to 0
@@ -146,7 +145,6 @@ in
       };
 
       dwindle = {
-        pseudotile = false;
         preserve_split = true;
         smart_resizing = true;
         use_active_for_splits = true;
