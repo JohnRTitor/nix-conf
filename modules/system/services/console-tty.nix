11521fa591295468344711590334c05366b76527
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-05-21 00:31:49 +0530

    console: add mouse support

--- modules/system/services/console-tty.nix
+++ modules/system/services/console-tty.nix
@@ -4,5 +4,5 @@
   # start after fs have been mounted, else it might fail
   # systemd.services.systemd-vconsole-setup.after = [ "local-fs.target" ];
 
-  # services.gpm.enable = true; # For mouse support in tty console, not needed for me
+  services.gpm.enable = true; # For mouse support in tty console
 }
