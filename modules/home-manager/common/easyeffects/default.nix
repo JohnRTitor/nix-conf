424a441362ba787056326ab5b6807f6f1d47d3a4
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-05-25 20:57:43 +0530

    update easyeffects

--- modules/home-manager/common/easyeffects/default.nix
+++ modules/home-manager/common/easyeffects/default.nix
@@ -7,7 +7,7 @@
       # GentleDynamics - https://github.com/droidwayin/GentleDynamics/blob/main/GentleDynamics.json
       base = builtins.fromJSON (builtins.readFile ./presets/base.json);
 
-      # EasyEffects - https://github.com/p-chan5/EasyPulse/blob/main/p-chan5/hifi-min.json
+      # Perfect EQ - https://github.com/JackHack96/EasyEffects-Presets/blob/master/Perfect%20EQ.json
       headphones = builtins.fromJSON (builtins.readFile ./presets/headphones.json);
 
       # Microphone with NR - https://gist.github.com/jtrv/47542c8be6345951802eebcf9dc7da31
